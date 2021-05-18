05f57c36c2157da4576a6daa0877e438d8d9fcad
Anandhakrishnan M <57976272+anandhakrishnanm@users.noreply.github.com>
2021-05-18 11:42:19 +0530

    UI bug fix in update password and Dashboard Routes (#10)
    * fix mobile view for update password
    
    * create isoStringToDate helper function
    
    * include all dashboard routes

--- src/helpers/date.js
+++ src/helpers/date.js
@@ -14,4 +14,28 @@ const convertDateFormate = (data) => {
   const convertedDate = `${newYear}-${newMonth}-${newDate}`;
   return convertedDate;
 };
-export { convertDateFormate }; // eslint-disable-line
+const isoStringToDate = (data) => {
+  const months = {
+    0: 'Jan',
+    1: 'Feb',
+    2: 'Mar',
+    3: 'Apr',
+    4: 'May',
+    5: 'Jun',
+    6: 'Jul',
+    7: 'Aug',
+    8: 'Sep',
+    9: 'Oct',
+    10: 'Nov',
+    11: 'Dec',
+  };
+  const date = new Date(data);
+  let day = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+  if (day < 10) {
+    day = `0${day}`;
+  }
+  return { day, month: months[month], year };
+};
+export { convertDateFormate, isoStringToDate };
