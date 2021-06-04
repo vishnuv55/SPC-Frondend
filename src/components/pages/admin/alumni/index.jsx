976188afb73bb6d259787769ad95ee0e1bf02999
anandhakrishnanm <anandhakrishnanmohank@gmail.com>
2021-06-04 21:43:59 +0530

    add heading for the Alumni page

--- src/components/pages/admin/alumni/index.jsx
+++ src/components/pages/admin/alumni/index.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './style.scss';
+import { useSetRhinoState } from '../../../../config/context';
 import CreateAlumni from './CreateAlumni';
 import DownloadAlumni from './DownloadAlumni';
 
 const Alumni = () => {
+  const setPageTitle = useSetRhinoState('pageTitle');
+  useEffect(() => {
+    setPageTitle('Manage Alumni Details');
+  }, [setPageTitle]);
   return (
     <div className="alumni-wrapper">
       <CreateAlumni />
