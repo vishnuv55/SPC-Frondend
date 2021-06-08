9db231a97297e048bec0c229f13db4750c292f5a
Vishnu Viswambharan <vishnuviswambharan55@gmail.com>
2021-06-08 17:57:25 +0530

    completed student creation by file upload feature (#21)
    * complete create students using file-upload
    
    * updated package.lock.json
    
    * added prettier
    
    * fix prettier build bug

---
+++ src/components/pages/admin/adminHome/CreateStudentByUpload/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './style.scss';
+import { FiUsers } from 'react-icons/fi';
+import FileUpload from './fileUpload';
+
+const CreateStudentByUpload = () => {
+  return (
+    <div className="student-file-upload">
+      <h5 className="heading-5">
+        <FiUsers className="icon" />
+        Create Student with upload
+      </h5>
+      <p className="paragraph">
+        The uploaded file must be of type text/csv with Register Number, Name, Email, Branch and
+        Number of backlogs Respectively.
+      </p>
+      <FileUpload />
+    </div>
+  );
+};
+
+export default CreateStudentByUpload;
--- src/components/pages/admin/adminHome/index.jsx
+++ src/components/pages/admin/adminHome/index.jsx
@@ -5,6 +5,7 @@ import { useSetRhinoState } from '../../../../config/context';
 import CreateStudent from './createStudent';
 import UpdateExecomPassword from './updateExecomPassword';
 import UpdateStudentPassword from './updateStudentPassword';
+import CreateStudentByUpload from './CreateStudentByUpload';
 
 const AdminHome = () => {
   const setPageTitle = useSetRhinoState('pageTitle');
@@ -17,6 +18,7 @@ const AdminHome = () => {
     <div className="admin-home">
       <div className="create-student-container">
         <CreateStudent />
+        <CreateStudentByUpload />
       </div>
       <div className="password-container">
         <UpdateStudentPassword />
